b2d970f1ad02e730258d9d716937ec35e418ed99
ValentinaStella <91681272+ValentinaStella@users.noreply.github.com>
2021-12-11 14:55:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    int posizione;
+    char a[10][20], f[1][20];
+    for (int i = 0; i < 10; ++i) {
+        cin>>a[i];
+
+    }
+    cin>>f[0];
+    if (cerca(a,f,posizione)){
+        cout<<posizione<<endl;
+    }else{
+        cout<<"non presente"<<endl;
+    }
+    return 0;
+
 }
