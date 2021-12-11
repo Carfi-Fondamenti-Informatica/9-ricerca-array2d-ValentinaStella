da3893f988f8fb128f682d397eeabe0350c2bf26
ValentinaStella <91681272+ValentinaStella@users.noreply.github.com>
2021-12-11 14:55:28 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
+#include <cstring>
+
+
+bool cerca (char elenco [10][20], char nome [1][20], int &posizione){
+    for (int i = 0; i < 10; ++i) {
+        if (strcmp(elenco[i], nome[0])==0){
+            posizione=i;
+        }
+
+    }
+    return false;
+}
 
